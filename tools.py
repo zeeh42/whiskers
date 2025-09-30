a974541dcd59cad6bfd8562c7dc2679de34e574d
zeeh42 <sxallum@gmail.com>
2025-09-30 23:58:23 +1300

    cleanup and wasted time

--- tools.py
+++ tools.py
@@ -6,23 +6,25 @@ class IO:
 
     @staticmethod
     def keyToValueDict(dict):
+        """returns a list where all the values are the keys"""
         newDict = {v: k for k, v in dict.items()}
         return newDict
 
 class Title:
+    """text to ascii class"""
 
     # Do not use default font
     fonts = {} # fonts = {"fontname":{"a":["  __  ", " / _\ ", "/    \\", "\_/\_/"]}}
 
     @staticmethod
-    def getTitle(fontname):
+    def getTitle(text, fontname,):
+        """returns an ascii banner as a string"""
         font = Title.fonts[fontname]
-        string = ""
-        
-        
+        return "TODO: get title as string"
 
     @staticmethod
     def loadFont(fontname):
+        """add a font to Title.fonts"""
         fontPath = C.Path.FONTDIRPATH / Path(fontname) 
 
         newFont = {}
@@ -34,12 +36,10 @@ class Title:
                 newFont.update({letterfile:lines})
         Title.fonts.update({fontname:newFont})
 
-
+    @staticmethod
     def removeFont(fontname):
+        """Removes a font from Title.fonts"""
         try:
             Title.fonts.pop(fontname)
         except KeyError:
             raise KeyError(f"{fontname} is not a font")
-
-Title.loadFont("mavic")
-print(Title.fonts["mavic"])
